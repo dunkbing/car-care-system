52ecc637afd9841fd9c36b97871e4afa268a4d4e
binh.buidang <dangbinh4869@gmail.com>
2021-10-25 15:32:31 +0700

    [add] smooth picker.

---
+++ src/components/SmoothPicker/functions/alignSelect.ts
@@ -0,0 +1,19 @@
+import { RefObject } from 'react';
+import { FlatList } from 'react-native';
+import { Option } from '../';
+
+export default function (horizontal: boolean | null, scrollAnimation: boolean, option: Option, refFlatlist: RefObject<FlatList>) {
+  try {
+    if (option) {
+      const newPosition = horizontal ? option.left + option.layout.width / 2 : option.top + option.layout.width / 2;
+      if (refFlatlist.current !== null) {
+        refFlatlist.current.scrollToOffset({
+          offset: newPosition,
+          animated: scrollAnimation,
+        });
+      }
+    }
+  } catch (e) {
+    console.log('error', e);
+  }
+}

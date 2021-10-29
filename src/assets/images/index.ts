fe8369446c3244810588f6f05d834c6247e1478f
nghia <nghianm2803@gmail.com>
2021-10-29 15:36:57 +0700

    [add] detail assign request screen

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,5 +4,3 @@ export { default as CurrentLocation } from './current-location.png';
 export { default as GoogleLogo } from './google-logo.png';
 export { default as DefaultCar } from './default-car.png';
 export { default as AvatarStaff } from './avatar-staff.png';
-export { default as Location } from './location.png';
-export { default as Call } from './call2.png';

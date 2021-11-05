162164843f8d4df6ae03a8068a85a5debf11830c
binh.buidang <dangbinh4869@gmail.com>
2021-11-06 04:08:04 +0700

    [add] create rescue detail request.

--- src/mobx/stores/rescue.ts
+++ src/mobx/stores/rescue.ts
@@ -3,12 +3,15 @@ import { STORE_STATES, USER_TYPES } from '@utils/constants';
 import { action, makeObservable, observable, runInAction } from 'mobx';
 import Container, { Service } from 'typedi';
 import RescueService from '@mobx/services/rescue';
-import { CustomerRescueHistoryModel, GarageRescueHistoryModel, RescueCase } from '@models/rescue';
+import { CustomerRescueHistoryModel, GarageRescueHistoryModel, RescueCase, RescueDetailRequest } from '@models/rescue';
 import AuthStore from './auth';
+import BaseStore from './base-store';
+import { withProgress } from '@mobx/services/config';
 
 @Service()
-export default class RescueStore {
+export default class RescueStore extends BaseStore {
   constructor() {
+    super();
     makeObservable(this, {
       state: observable,
       customerRescueHistories: observable,
@@ -21,11 +24,15 @@ export default class RescueStore {
   private readonly rescueService = Container.get(RescueService);
   private readonly authStore = Container.get(AuthStore);
 
-  state: STORE_STATES = STORE_STATES.IDLE;
   customerRescueHistories: Array<CustomerRescueHistoryModel> = [];
   garageRescueHistories: Array<GarageRescueHistoryModel> = [];
   rescueCases: Array<RescueCase> = [];
 
+  /**
+   * get current user's rescue histories.
+   * @param keyword
+   * @param userType
+   */
   public async find(keyword: string, userType: USER_TYPES = USER_TYPES.CUSTOMER) {
     this.state = STORE_STATES.LOADING;
 
@@ -60,21 +67,41 @@ export default class RescueStore {
     }
   }
 
+  /**
+   * get a list of common rescue cases.
+   */
   public async getRescueCases() {
-    this.state = STORE_STATES.LOADING;
+    this.startLoading();
 
-    const { result, error } = await this.rescueService.getRescueCases();
+    const { result, error } = await withProgress(this.rescueService.getRescueCases());
 
     if (error) {
-      runInAction(() => {
-        this.state = STORE_STATES.ERROR;
-      });
+      this.handleError(error);
     } else {
       const rescues = result || [];
       runInAction(() => {
-        this.state = STORE_STATES.SUCCESS;
         this.rescueCases = rescues;
       });
+      this.handleSuccess();
+    }
+  }
+
+  /**
+   * create a rescue case.
+   * @param rescueDetail
+   */
+  public async createRescueCase(rescueDetail: RescueDetailRequest) {
+    this.startLoading();
+
+    const { result, error } = await this.rescueService.createRescueDetail(rescueDetail);
+
+    if (error) {
+      this.handleError(error);
+    } else {
+      runInAction(() => {
+        this.rescueCases.push(result);
+      });
+      this.handleSuccess();
     }
   }
 }

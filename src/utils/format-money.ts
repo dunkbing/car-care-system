9c4ac31df0174201f45cef907411d9064d9a1e3f
Bùi Đặng Bình <dangbinh4869@gmail.com>
2021-11-16 21:31:38 +0700

    [update] format money.

--- src/utils/format-money.ts
+++ src/utils/format-money.ts
@@ -1,8 +1,3 @@
 export default function formatMoney(money: number): string {
-  const formatter = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  });
-
-  return formatter.format(money);
+  return `${money.toLocaleString()}đ`;
 }

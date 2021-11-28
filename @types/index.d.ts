60f88d15ad25ae4a0ba1af52be8c844dae00f9f9
Bùi Đặng Bình <dangbinh4869@gmail.com>
2021-11-28 14:41:32 +0700

    [add] noti-server.

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -2,6 +2,7 @@ declare module '@env' {
   export const API_URL: string;
   export const GOONG_API_KEY: string;
   export const GOONG_MAP_TILE_KEY: string;
+  export const NOTI_SERVER: string;
 }
 
 declare module '*.png' {
